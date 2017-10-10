7e27bead2941be8b9a4fdc6ba64b6d9df9c108d3
Romanas Sonkinas <romanass@gmail.com>
2017-10-10 17:00:48 +0100

    Test mode

--- util/app.c
+++ util/app.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include "telehash.h"
 #include "net_tcp4.h"
+#include "net_loopback.h"
 
 chan_t stream_send(chan_t chan, uint8_t * data, size_t size);
+void parse_args(int argc, char ** argv);
+void test_loopback(mesh_t meshA, mesh_t meshB);
 
-static char * reply_prefix = "REPLY: ";
+//static char * reply_prefix = "REPLY: ";
+
+static int loopback = 0;
+static int static_keys = 0;
 
 void imont_stream_chan_handler(chan_t chan, void *arg) {
     lob_t packet;
     if(!chan) return;
 
-    while((packet = chan_receiving(chan)))
-    {
+    //if(!(open = chan_open(chan))) return; // paranoid
+    chan->state = CHAN_OPEN;
+    while((packet = chan_receiving(chan))) {
+        //lob_free(lob_unlink(open));
         printf("Received new packet with sequence: %d, body length is: %zd\n", lob_get_int(packet, "seq"), packet->body_len);
         if (packet->body_len > 0) {
             //size_t reply_length = packet->body_len + strlen(reply_prefix) + 1;
@@ -21,10 +29,16 @@ void imont_stream_chan_handler(chan_t chan, void *arg) {
             //reply = strncat(reply, (const char *) packet->body, packet->body_len);
             //printf("Replying with: %s\n", reply);
             //stream_send(chan, (uint8_t *) reply, strlen(reply));
-            stream_send(chan, (uint8_t *) reply_prefix, strlen(reply_prefix));
+//            uint8_t repl[] = {88, 88, 88, 88};
+//            if (!stream_send(chan, repl, 4)) {
+//                printf("Sending failed! \n");
+//            } else {
+//                printf("SENT\n");
+//            }
             //free(reply);
             //free(received);
         }
+        lob_free(packet);
     }
 
 }
@@ -57,7 +71,8 @@ chan_t stream_new(link_t link) {
 
 chan_t stream_send(chan_t chan, uint8_t * data, size_t size) {
     lob_t pkt = chan_packet(chan);
-    lob_body(pkt, data, size);
+    //lob_body(pkt, data, size);
+    //lob_set
     if (!chan_send(chan, pkt)) {
         return NULL;
     }
@@ -67,14 +82,14 @@ chan_t stream_send(chan_t chan, uint8_t * data, size_t size) {
 mesh_t load_static();
 
 int main(int argc, char ** argv) {
-    //util_sys_logging(1);
+    util_sys_logging(0);
     printf("Test App\n");
-    mesh_t mesh = mesh_new(11);
+    parse_args(argc, argv);
+
+    mesh_t mesh;
     lob_t secrets, json;
-    if (argc > 1) {
-        if (strcmp(argv[1], "static") == 0) {
-            mesh = load_static();
-        }
+    if (static_keys) {
+        mesh = load_static();
         secrets = NULL;
     } else {
         mesh = mesh_new(11);
@@ -93,11 +108,25 @@ int main(int argc, char ** argv) {
 
     mesh_on_open(mesh, "stream", imont_stream_on_open);
 
-    lob_t tcp_options = lob_new();
-    lob_set_int(tcp_options, "port", 4567);
-    net_tcp4_t net = net_tcp4_new(mesh, tcp_options);
-    printf("Listening on port: %d\n", net->port);
-    while(net_tcp4_loop(net));
+    if (loopback) {
+        mesh_t meshB = mesh_new(13);
+        lob_t secretsB = mesh_generate(meshB);
+        test_loopback(mesh, meshB);
+        mesh_free(meshB);
+        lob_free(secretsB);
+    } else {
+        lob_t tcp_options = lob_new();
+        lob_set_int(tcp_options, "port", 4567);
+        net_tcp4_t net = net_tcp4_new(mesh, tcp_options);
+        printf("Listening on port: %d\n", net->port);
+        while(net_tcp4_loop(net));
+        lob_free(tcp_options);
+        net_tcp4_free(net);
+    }
+
+    mesh_free(mesh);
+    lob_free(json);
+    lob_free(secrets);
     printf("Exiting \n");
     return 0;
 }
@@ -111,4 +140,33 @@ mesh_t load_static() {
     lob_set(secrets, "1a", "aa6ltuce4k5v4a5x57hlljfhgqvccopp3e");
     mesh_load(mesh, secrets, keys);
     return mesh;
+}
+
+void parse_args(int argc, char** argv) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "static") == 0) {
+            static_keys = 1;
+        }
+        if (strcmp(argv[i], "loopback") == 0) {
+            loopback = 1;
+        }
+    }
+}
+
+void test_loopback(mesh_t meshA, mesh_t meshB) {
+    printf("Will test loopback \n");
+    mesh_on_open(meshB, "stream", imont_stream_on_open);
+
+    net_loopback_t loop = net_loopback_new(meshA, meshB);
+    link_t linkBA = link_get(meshB, meshA->id);
+    chan_t stream = stream_new(linkBA);
+    for (uint8_t x = 0; x < 1; x++) {
+        uint8_t msg[] = {72, 72, 69, 69, x};
+        stream_send(stream, msg, 5);
+        lob_t rcv;
+        while ((rcv = chan_receiving(stream))) {
+            printf("Received reply with id %d\n", rcv->id);
+        }
+    }
+    net_loopback_free(loop);
 }
